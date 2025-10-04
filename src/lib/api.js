ae69abbab8731943d48bd14b2596ee379ffc3c66
yalindi <yalindidasanya@gmail.com>
2025-10-04 21:39:35 +0530

    fix:fixing meta details

--- src/lib/api.js
+++ src/lib/api.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ 
-    baseUrl: `${import.meta.env.VITE_BACKEND_URL}/api`,
+    baseUrl: import.meta.env.VITE_BACKEND_URL,
     prepareHeaders: async (headers) => {
       return new Promise((resolve) => {
         async function checkToken() {
