f60d872a6b75bd375657973948ef59fb476a5353
yalindi <yalindidasanya@gmail.com>
2025-10-06 16:53:28 +0530

    fix:deployment issue

--- src/components/HotelList.jsx
+++ src/components/HotelList.jsx
@@ -1,7 +1,6 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 import { MapPin, Star, Wifi, Coffee, Car } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import { Badge } from '@/components/ui/badge';
 
 const HotelList = ({ hotels }) => {
   return (
